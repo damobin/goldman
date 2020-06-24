bd5f024ddb35dc451e3cdb5735b15c1ef3fe3341
mo <286711902@qq.com>
2020-06-24 13:00:25 +0800

    绘制goldman的分数面板,绘制人物，绘制人物背景

--- goldman/main.cpp
+++ goldman/main.cpp
@@ -18,12 +18,16 @@ enum MINE {
 	i_bk		= PICTURE_NUM-1,
 
 };
+struct stgoldman {
+	int posx, posy;		//pos
+	int width, height;	//sharp
+	int coin;			//gold
+};
 
 
-
-//self data struct
-IMAGE imgPicture[PICTURE_NUM];
 //var declear
+IMAGE imgPicture[PICTURE_NUM];
+stgoldman st_goldman;
 
 //funs declear
 void initGame();
@@ -46,10 +50,32 @@ void initGame()
 		loadimage(&imgPicture[i], c8ImgPath);
 	}
 	loadimage(&imgPicture[i_bk],"./images/bk.jpg");
+
+	st_goldman.posx   =  WINDOWWIDTH / 2 - st_goldman.width / 2;
+	st_goldman.posy	  = 0;
+	st_goldman.width  = 140;
+	st_goldman.height = 120;
+	st_goldman.coin   = 0;
+	//绘制goldman背景颜色
+
 }
+#define SCORE_POSX	20
+#define SCORE_POSY	20
 void drawGame()
 {
 	cout << "Test" << endl;
 	putimage(0, 120, &imgPicture[i_bk]);
+	//背景颜色填充
+	setfillcolor(RGB(206, 141, 59));
+	solidrectangle(0, 0, WINDOWWIDTH, 120);
+	putimage(st_goldman.posx, st_goldman.posy, &imgPicture[i_soledown-1],SRCAND);
+	putimage(st_goldman.posx, st_goldman.posy, &imgPicture[i_soledown],SRCPAINT  );
 	
+	//绘制分数面板
+	char score[30] = "";
+	sprintf_s(score, "分数:%06d", st_goldman.coin);
+	setfillcolor(WHITE);	    //字体填充颜色
+	setbkmode(TRANSPARENT);		//文本框背景填充颜色
+	settextstyle(50, 0, "宋体");//字体
+	outtextxy(SCORE_POSX, SCORE_POSY, score);	//输出
 }
